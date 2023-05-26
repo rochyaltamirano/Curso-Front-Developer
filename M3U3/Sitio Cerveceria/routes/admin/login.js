bdcede94cba2f333a7f3f60f4a23435f08b14dd5
rochyaltamirano <rochyaltamirano@gmail.com>
2023-05-26 17:14:31 -0300

    Incorporación de sistema de login y logout en Cervecería

---
+++ M3U3/Sitio Cerveceria/routes/admin/login.js
@@ -0,0 +1,44 @@
+var express = require ('express');
+var router = express.Router();
+var usuariosModel = require ('./../../models/usuariosModel');
+
+router.get('/', function(req, res, next){
+    res.render('admin/login', {
+        layout: 'admin/layout'
+    });
+});
+
+/*para destruir variables de session*/
+router.get('/logout', function(req, res, next) {
+    req.session.destroy(); //destruir
+    red.render ('admin/login', {
+        layout: 'admin/layout'
+    });
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        var usuario = req.body.usuario; //capturamos datos y los guardamos en la variable
+        var password = req.body.password; //idem
+
+        console.log(req.body);
+
+        var data = await usuariosModel.getUserAndPassword (usuario, password);
+
+        if (data != undefined){
+            req.session.id_usuario = data.id; // id > nombre de la columna
+            req.session.nombre = data.usuario;
+            
+            res.redirect('/admin/novedades');
+        } else {
+            res.render ('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            })
+        }//cierre else
+    } catch (error) {
+        console.log(error);
+    }
+}); //cierra el post
+
+module.exports = router;
